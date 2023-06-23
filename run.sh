6e5a8b80001b5812757edec5be7639db8ee48b3c
mohammed boukhal <lios80466@gmail.com>
2023-06-23 19:03:49 +0100

    Update -- add exection path

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Pull if there is any changes
-sudo git pull origin master
+installPath='/usr/bin/gateRp'
 
-sudo python3 main.py
+# Pull if there is any changes
+sudo git config --global --add safe.directory $installPath
+sudo git pull origin master --no-rebase
+sudo python3 $installPath/src/main.py
