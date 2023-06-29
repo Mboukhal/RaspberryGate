98727194c590d34eed7c9504ae2779442d6c1bd5
Mohammed Boukhal <85312140+Mboukhal@users.noreply.github.com>
2023-06-29 05:19:02 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while ! ping -c 1 google.com >/dev/null 2>&1; do
-    :
-done
+# while ! ping -c 1 google.com >/dev/null 2>&1; do
+#     :
+# done
 
 # Pull if there is any changes
 sudo git config --global --add safe.directory $installPath
