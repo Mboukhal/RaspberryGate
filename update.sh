33938f797bf5a46f0c52c50cf9505ab9b8dd54e0
Mohammed Boukhal <85312140+Mboukhal@users.noreply.github.com>
2023-06-29 05:18:19 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,6 @@ while ! ping -c 1 google.com >/dev/null 2>&1; do
     :
 done
 
-
+# Pull if there is any changes
+sudo git config --global --add safe.directory $installPath
+sudo git pull origin master --no-rebase
