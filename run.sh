8b5bcd0adc7e3971a34a358d1dce4549ac9728ff
Mohammed Boukhal <85312140+Mboukhal@users.noreply.github.com>
2023-06-29 05:17:09 +0100

    Update -- run update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 installPath='/usr/bin/gateRp'
 
-# Pull if there is any changes
-sudo git config --global --add safe.directory $installPath
-sudo git pull origin master --no-rebase
+chmod +x update.sh
+bash update.sh &
+
 sudo python3 $installPath/src/main.py
